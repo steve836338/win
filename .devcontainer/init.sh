007c45914b7f79107e5a41404ace8138019c55cc
steve836338 <stevejobs7654@mailto.frii.site>
2026-09-24 15:18:21 +0000

    Create init.sh

---
+++ .devcontainer/init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+mkdir -p vms
+cat > windows.yml <<'EOF'
+version: "3.9"
+services:
+  windows:
+    image: dockurr/windows
+    container_name: windows
+    environment:
+      VERSION: "11"
+      USERNAME: "User"
+      PASSWORD: "123"
+      RAM_SIZE: "8G"
+      CPU_CORES: "4"
+    devices:
+      - /dev/kvm
+      - /dev/net/tun
+    cap_add:
+      - NET_ADMIN
+    ports:
+      - "8006:8006"
+      - "3389:3389/tcp"
+      - "3389:3389/udp"
+    volumes:
+      - ./vms:/storage
+    restart: always
+    stop_grace_period: 2m
+EOF
